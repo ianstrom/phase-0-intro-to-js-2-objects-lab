07858e5f80fc31d54d6640d4d5a694a454531b08
ianstrom <iankstrom@gmail.com>
2022-10-30 15:47:53 -0700

    commit tree d19193e54a0618954faee887db6363a4dd079e2d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: 'Ian',
+  streetAddress: '11 Oxford Terrace',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+ const employee2 = {...employee};
+ employee2[key] = value;
+ return employee2
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value
+  return employee
+}
+ 
+function deleteFromEmployeeByKey(employee, key) {
+  const employee2 = {...employee}
+  delete employee2.name
+  return employee2
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee.name;
+  return employee;
+}
